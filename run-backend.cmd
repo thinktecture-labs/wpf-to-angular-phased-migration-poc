8a8b5851e9c01f02db7cb0cac78ee6bf50bae145
Kenny Pflug <kenny.pflug@live.de>
2023-04-06 17:00:04 +0200

    run-backend.cmd now also builds and copies the Angular client

--- run-backend.cmd
+++ run-backend.cmd
@@ -1,2 +1,7 @@
 dotnet build ./Backend/Backend.csproj
+cd ./angular-app
+CALL npm install
+CALL ng build --configuration production
+cd ..
+XCOPY angular-app\dist\angular-app Backend\bin\Debug\net7.0\angular-app /s /i /y
 dotnet run  --project ./Backend/Backend.csproj --no-build
